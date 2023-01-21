6b14e329226a97fec6e8fe185074e6f9a2c0bf91
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2023-01-21 23:58:46 +0300

    Integration of skeleton for preload of the data about products.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Categories from './components/Categories';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
 import { useEffect, useState } from 'react';
+import Preloader from './components/PizzaBlock/Skeleton';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://63cae056f36cbbdfc76246cf.mockapi.io/items')
       .then(res => res.json())
-      .then(jsonArray => setItems(jsonArray)
-      )
+      .then(jsonArray => {
+        setItems(jsonArray);
+        setIsLoading(false);
+      })
   }, [])
 
   return (
@@ -28,9 +32,11 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              items.map(pizzasItem => {
-                return <PizzaBlock key={pizzasItem.id} {...pizzasItem} />
-              })
+              isLoading
+                ? [...new Array(6)].map((_, index) => <Preloader key={index} />)
+                : items.map(pizzasItem => {
+                  return <PizzaBlock key={pizzasItem.id} {...pizzasItem} />
+                })
             }
           </div>
         </div>
