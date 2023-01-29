09a3ffa1126b8b525cbd899c84ea3bd9ddc7dbab
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2023-01-29 14:46:11 +0300

    Implemented redux logic of the cart.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Search from './Search/Search';
 
 function Header() {
+    const { totalPrice, totalCount } = useSelector(state => state.cart);
+
     return (
         <div className="header">
             <div className="container">
@@ -18,7 +22,7 @@ function Header() {
                 <Search />
                 <div className="header__cart">
                     <Link to="/cart" className="button button--cart">
-                        <span>520 ₽</span>
+                        <span>{totalPrice} ₽</span>
                         <div className="button__delimiter"></div>
                         <svg
                             width="18"
@@ -61,7 +65,7 @@ function Header() {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>3</span>
+                        <span>{totalCount}</span>
                     </Link>
                 </div>
             </div>
