36a265caba30ba916809c56b92e347390969d8a3
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2023-01-30 21:11:06 +0300

    Implemented display of extended information about selected pizza.

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { addItem } from "../../redux/slices/cartSlice";
 import { selectCartItemById } from "../../redux/slices/selectors/cartSelectors";
 
@@ -32,12 +33,14 @@ function PizzaBlock({ id, title, price, imageUrl, types, sizes }) {
     return (
         <div className="pizza-block-wrapper">
             <div className="pizza-block">
+            <Link to={`/pizza/${id}`}>
                 <img
                     className="pizza-block__image"
                     src={imageUrl}
                     alt="Pizza"
                 />
                 <h4 className="pizza-block__title">{title}</h4>
+                </Link>
                 <div className="pizza-block__selector">
                     <ul>
                         {
@@ -67,7 +70,10 @@ function PizzaBlock({ id, title, price, imageUrl, types, sizes }) {
                             xmlns="http://www.w3.org/2000/svg"
                         >
                             <path
-                                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 
+                                4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 
+                                7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 
+                                10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                                 fill="white"
                             />
                         </svg>
